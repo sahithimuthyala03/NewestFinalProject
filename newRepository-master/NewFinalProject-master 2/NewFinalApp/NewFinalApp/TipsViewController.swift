2042d42e9a5b8904bc0abf9a6fab55bd40a4bdc1
sahithimuthyala03 <69254657+sahithimuthyala03@users.noreply.github.com>
2020-08-12 15:05:17 -0400

    Add files via upload

---
+++ newRepository-master/NewFinalProject-master 2/NewFinalApp/NewFinalApp/TipsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TipsViewController.swift
+//  NewFinalApp
+//
+//  Created by Apple on 8/12/20.
+//  Copyright © 2020 Meghan Jachna. All rights reserved.
+//
+
+import UIKit
+
+class TipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
